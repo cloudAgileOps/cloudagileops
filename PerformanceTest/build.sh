a1b698213a269b1aa83cf21beb860898d9e0d0ac
Root <root@mo-daa74f754.mo.sap.corp>
2019-02-27 12:07:07 +0000

    add JMeter Dockerfile

---
+++ PerformanceTest/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "$1" ==  "" ] ; then 
+
+echo "usage build.sh <tag>"
+exit 1
+
+fi
+
+
+echo "tag: $1"
+
+cp -pr ../toDoListPro toDoListPro 
+
+docker build . -t $1
+
+
+rm -rf toDoListPro
